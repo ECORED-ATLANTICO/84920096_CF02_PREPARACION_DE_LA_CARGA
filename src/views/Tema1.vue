0d006db94f64ed927ffb030d506c2f6474762a93
AdonadoMolinares <donadomolinares@gmail.com>
2025-07-11 11:53:50 -0500

    commit

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -5,8 +5,233 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 1
-      h1 Aseguramiento de la carga 
- 
+      h1 Aseguramiento de la carga
+    .bg_img_1.mb-3
+      .bloque-texto-t.justify-content-center.align-items-center.mb-0
+        .bloque-texto-t__img.mb-0
+          img(src='@/assets/curso/temas/tema_1/img_1.png')
+        .bloque-texto-t__texto.color-adicional-gradient.col-10.mb-0
+          p.mb-0 Es fundamental asegurarse que la carga esté bien asegurada en cualquier proceso logístico; y más precisan cuando se trata de materiales con montacargas. 
+          p.mb-0 Si se lleva a cabo de forma adecuada, no solo protege los objetos que se traslada, sino que también se evitan accidentes, se conserva la integridad del equipo y se aumenta la eficiencia en bodegas, obras y centros de distribución. 
+
+      separador
+
+      #t_1_1.titulo-segundo.color-acento-contenido
+        .h2(data-aos="fade-right") 1.1	Concepto
+      .mb-3
+      
+      .tarjeta.tarjeta--fondo__slider.p-4.mb-5
+        SlyderA(tipo="b").tarjeta.tarjeta--blanca.p-4
+          .row.justify-content-center.align-items-center
+            .col-12.col-lg-6.order-2.order-lg-1.mb-0
+              p.mb-0 El aseguramiento de la carga es un proceso técnico que integra métodos y procedimientos específicos orientados a fijar, estabilizar y proteger la mercancía durante las fases de transporte, almacenamiento y manipulación. 
+              p.mb-0 Su propósito principal es garantizar que los productos lleguen a su destino final en condiciones óptimas, evitando daños, pérdidas o incidentes ocasionados por factores como movimientos bruscos, vibraciones, impactos o condiciones climáticas adversas.
+            .col-12.col-lg-4.order-1.order-lg-2.mb-lg-0.mb-3
+              figure
+                img(src='@/assets/curso/temas/tema_1/img_2.png', class="img-fluid")
+          .row.justify-content-center.align-items-center
+            .col-12.col-lg-6.order-2.order-lg-1.mb-0
+              p.mb-0 Se requiere el uso adecuado de materiales y sistemas de sujeción (como amarres, cinchas, esquineros, entre otros), así como una correcta distribución del peso dentro del vehículo o contenedor, de modo que se preserve su estabilidad y se minimicen los riesgos operativos.
+              p.mb-0 Esta práctica no solo contribuye a la eficiencia de la logística, sino que también permite el cumplimiento de normativas nacionales e internacionales de seguridad en el transporte de carga.
+            .col-12.col-lg-4.order-1.order-lg-2.mb-lg-0.mb-3
+              figure
+                img(src='@/assets/curso/temas/tema_1/img_3.png', class="img-fluid")
+    .row.d-flex.align-items-stretch.justify-content-center.mb-5
+      .col-12.col-lg-4.d-flex.mb-lg-0.mb-3
+        figured-flex.flex-column.justify-content-center.align-items-center.w-100.h-100
+          img(src='@/assets/curso/temas/tema_1/img_4.png', class="img-fluid h-100")
+      .col-12.col-lg-8.d-flex
+        .d-flex.flex-column.justify-content-center.align-items-center.w-100.h-100
+          .tarjeta_img_1.w-100.h-100
+            p.mb-0 Cuando una carga no se asegura de forma adecuada, se generan riesgos que pueden comprometer tanto la integridad de la mercancía como la seguridad del personal y la eficiencia operativa. 
+            p.mb-0 Estas consecuencias afectan directamente la rentabilidad, la logística y la reputación de las empresas involucradas en la cadena de suministro.
+
+    .bg_img_2.mb-3
+      .row.justify-content-center.mb-5
+        .col-12.col-lg-3.mb-4.mb-xl-0
+          .crd_hover_txt(data-aos="flip-left")
+            .crd_hover_txt--img
+              figure
+                img(src="@/assets/curso/temas/tema_1/img_6.png")
+          .crd_hover_txt--body
+            h4.mb-3.text-center Daños  en la mercancía
+            p.mb-0.text-small Cuando los materiales se mueven de forma insegura, el impacto o la vibración durante el transporte pueden causar roturas, deformaciones o pérdida de valor en los productos.
+        .col-12.col-lg-3.mb-4.mb-xl-0
+          .crd_hover_txt(data-aos="flip-left")
+            .crd_hover_txt--img
+              figure
+                img(src="@/assets/curso/temas/tema_1/img_7.png")
+          .crd_hover_txt--body
+            h4.mb-3.text-center Accidentes laborales
+            p.mb-0.text-small Cargas inestables o mal sujetas pueden caer, provocando lesiones graves a los operadores de montacargas o al personal cercano.
+        .col-12.col-lg-3.mb-4.mb-xl-0
+          .crd_hover_txt(data-aos="flip-left")
+            .crd_hover_txt--img
+              figure
+                img(src="@/assets/curso/temas/tema_1/img_8.png")
+          .crd_hover_txt--body
+            h4.mb-3.text-center Retrasos en la cadena de suministro
+            p.mb-0.text-small Una carga mal asegurada puede provocar caídas o daños durante el transporte, lo que inevitablemente causa retrasos. Cuando un producto no llega a tiempo, toda la operación se ve afectada.
+        .col-12.col-lg-3.mb-4.mb-xl-0
+          .crd_hover_txt(data-aos="flip-left")
+            .crd_hover_txt--img
+              figure
+                img(src="@/assets/curso/temas/tema_1/img_9.png")
+          .crd_hover_txt--body
+            h4.mb-3.text-center Costos adicionales de reemplazo y mantenimiento
+            p.mb-0.text-small Cuando los materiales se dañan durante el transporte, la empresa debe asumir gastos adicionales, como: reemplazo de productos afectados y reparación de equipos dañados.
+      .row.justify-content-center.align-items-center.mb-5
+        .col-12.col-lg-10
+          .cajon.color-primario-adicional-2.p-4.mb-3
+            p.mb-0 El aseguramiento de la carga bien establece un proceso estructurado que ofrece a la carga estabilidad y seguridad durante su traslado. 
+            p.mb-0 Los siguientes pasos claros incluyen:
+    .row.d-flex.align-items-start.justify-content-center.mb-3(data-aos="zoom-in")
+      .col-10.col-lg-4.d-flex.mb-lg-0.mb-3
+        figure.d-flex.flex-column.justify-content-center.align-items-center.w-100.h-100
+          img(src='@/assets/curso/temas/tema_1/img_10.png', class="img-fluid h-100")
+      .col-12.col-lg-8.mb-lg-0.mb-3
+        LineaTiempoD.color-adicional-3
+          .row(numero="1" titulo="Evaluación de la carga")
+            .col-md-12.mb-4.mb-md-0
+              p.text-small.mb-0
+          .row(numero="2" titulo="Selección del método de aseguramiento")
+            .col-md-12.mb-4.mb-md-0
+              p.text-small.mb-0
+          .row(numero="3" titulo="Preparación del soporte de carga")
+            .col-md-12.mb-4.mb-md-0
+              p.text-small.mb-0
+          .row(numero="4" titulo="Distribución y estiba")
+            .col-md-12.mb-4.mb-md-0
+              p.text-small.mb-0
+          .row(numero="5" titulo="Aplicación de técnicas de fijación")
+            .col-md-12.mb-4.mb-md-0
+              p.text-small.mb-0
+          .row(numero="6" titulo="Verificación final")
+            .col-md-12.mb-4.mb-md-0
+              p.text-small.mb-0
+          .row(numero="7" titulo="Documentación y cumplimiento")
+            .col-md-12.mb-4.mb-md-0
+              p.text-small.mb-0
+
+    separador
+
+    #t_1_2.titulo-segundo.color-acento-contenido
+      .h2(data-aos="fade-right") 1.2	Métodos
+    .mb-3
+
+    .bg_img_3.mb-3
+      p.mb-0 Existen múltiples procedimientos para garantizar la carga a utilizar dependiendo el objeto comercial y el medio de transporte, adaptándose a una cuestión de fragilidad, peso, tamaño y de normas complementarias. 
+      p.mb-5 A continuación, se describen los principales métodos de aseguramiento de la carga:
+
+      .titulo-sexto.color-acento-contenido
+        h5 Tabla 1.
+        |  Métodos de aseguramiento de la carga
+      .tabla-a.mb-5
+        table
+          caption 
+            p <b>Fuente:</b> SENA, 2025.
+          thead
+            tr
+              th Método
+              th Descripción
+              th Materiales
+              th Aplicación
+          tbody
+            tr
+              td Bloqueo y calce 
+              td Técnica que utiliza elementos rígidos o semirrígidos para rellenar espacios vacíos y evitar el movimiento de la carga.
+              td Madera (tacos, vigas y listones). <br>Espumas de poliuretano o poliestireno. <br>Separadores plásticos o metálicos. <br>Bolsas de aire inflables.
+              td Transporte de maquinaria pesada. <br>Carga de vidrio.
+            tr
+              td Trincaje (Amarre)
+              td Método de sujeción mediante elementos tensores (cinchas, cadenas o cables) que fijan la carga a estructuras rígidas del vehículo o contenedor.
+              td Cinchas de poliéster o nylon. <br>Cadenas de acero con tensores. <br>Cables de acero con ganchos.
+              td Transporte de vehículos. <br>Carga en barcos.
+            tr
+              td Encintado y flejado
+              td Técnica de consolidación de paquetes o bultos mediante cintas adhesivas o flejes metálicos / plásticos.
+              td Cinta adhesiva reforzada. <br>Flejes de poliéster o acero. <br>Flejadoras manuales o automáticas.
+              td Cajas de cartón. <br>Paquetes de tubos metálicos.
+            tr
+              td Uso de rellenos
+              td Empleo de materiales de amortiguación para proteger la carga de impactos y vibraciones.
+              td Burbujas de aire. <br>Espuma de poliestireno. <br>Papel <I>kraft</I> o rellenos biodegradables.
+              td Electrónicos. <br>Muebles frágiles.
+
+            tr
+              td Sistemas modulares (paletización)
+              td Agrupación de carga sobre paletas para facilitar su manejo con equipos mecánicos (montacargas, transpaletas, entre otros).
+              td Paletas de madera, plástico o metal. <br><I>Film</I> estirable. <br>Cubiertas termorretráctiles.
+              td Almacenamiento de alimentos. <br>Exportación de químicos.
+
+
+    separador
+
+    #t_1_3.titulo-segundo.color-acento-contenido
+      .h2(data-aos="fade-right") 1.3	Normativa
+    .mb-3
+
+    .bg_img_4.mb-3
+      .row.justify-content-center.align-items-center.mb-3
+        .col-12.col-lg-4.mb-lg-0.mb-3
+          figure
+            img(src='@/assets/curso/temas/tema_1/img_11.png', class="img-fluid")
+        .col-12.col-lg-8
+          .cajon.color-primario-adicional-2.p-3.mb-3
+            p.mb-0 El aseguramiento de carga está regulado por estándares internacionales, regionales y nacionales para garantizar la seguridad en el transporte, prevenir accidentes y asegurar el cumplimiento de requisitos legales.
+          p.mb-0 Para la seguridad y eficiencia en las operaciones logísticas, el cumplimiento de las normativas nacionales e internacionales es fundamental. En Colombia, el Ministerio de Transporte, la DIAN y la Superintendencia de Puertos coercitiva protegen el aseguramiento de carga correctamente con multas para quien no cumpla. Las empresas deben entregar la formación necesaria a su plantel y trabajar con materiales certificados, bancales de riesgo legal y operacional. 
+          p.mb-0 A continuación, se detallan las principales normativas aplicables:
+
+      .titulo-sexto.color-acento-contenido
+        h5 Tabla 2.
+        |  Normativa para el aseguramiento de la carga
+      .tabla-a.mb-5
+        table
+          caption 
+            p <b>Fuente:</b> SENA, 2025.
+          thead
+            tr
+              th Normas
+              th Descripción
+              th Objetivo
+          tbody
+            tr
+              td(rowspan="4") Internacionales
+              td OMI (Organización Marítima Internacional) - Código CTU
+              td Establecer directrices para el correcto estibado y trincaje de carga en contenedores y unidades de transporte.tr
+            tr
+              td Normas ISO <br>ISO 1496 <br>ISO 17712 <br>ISO 3874
+              td Especificaciones técnicas para contenedores de carga. <br>Requisitos para sellos de seguridad en contenedores. <br>Estándares para manipulación de contenedores.
+            tr
+              td DOT (Departamento de Transporte - EE.UU.)
+              td Regula el peso máximo, distribución y sujeción de carga en transporte terrestre. <br>Exige el uso de cinchas con resistencia certificada.
+            tr
+              td Unión Europea - Reglamento UE 2014/47
+              td Establece requisitos técnicos para vehículos de carga. <br>Incluye inspecciones periódicas de sistemas de amarre. <br>Exige que la carga no sobresalga de los límites del vehículo.
+
+            tr
+              td(rowspan="3") Regionales
+              td CEMT (Conferencia Europea de Ministros de Transporte)
+              td Fija límites de peso y dimensiones para camiones en Europa. <br>Promueve estándares de seguridad en logística internacional.
+
+            tr
+              td Resolución 4100 de 2004 (Ministerio de Transporte)
+              td Establece las condiciones técnicas para el transporte de carga por carretera. <br>Exige que la carga esté correctamente asegurada para evitar desplazamientos. <br>Prohíbe el transporte de mercancías sueltas sin sujeción adecuada.
+
+            tr
+              td Decreto 1079 de 2015 (Reglamento de Tránsito)
+              td Obliga a los conductores a garantizar la estabilidad de la carga. <br>Impone multas por carga mal asegurada que represente riesgo vial.
+
+            tr
+              td(rowspan="3") Colombianas
+              tdNormas Icontec <br>NTC 5205 <br>NTC 1695
+              td Requisitos para flejes metálicos en carga. <br>Estándares para embalajes de madera
+            tr
+              td Superintendencia de Puertos y Transporte
+              td Realiza operativos de control para verificar el correcto aseguramiento de carga. <br>Aplica sanciones por incumplimiento.
+            tr
+              td DIAN
+              td Exige que la carga en contenedores cumpla con normas internacionales para exportaciones. <br>Rechaza mercancías mal estibadas que puedan sufrir daños en tránsito.
 </template>
 
 <script>
