bf1a3f54422df8224664d6ee49aa24cef3b83994
AdonadoMolinares <donadomolinares@gmail.com>
2025-09-09 11:08:56 -0500

    commit

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -22,7 +22,7 @@ export default {
       introduccion:
         '<b> Objetivo:</b> validar el conocimiento adquirido sobre el aseguramiento y embalaje de la carga.',
       barajarPreguntas: true,
-      titulo_aprobado: '¡BUEN TRABAJO!..',
+      titulo_aprobado: '¡BUEN TRABAJO!.',
       titulo_reprobado: 'VUELVA A INTENTARLO.',
       preguntas: [
         {
